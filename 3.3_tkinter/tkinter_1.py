2e92fe722115fa72e7c2ebc301c868f905f3748e
Ali Fazeli <a.fazeli95@gmail.com>
2017-07-19 11:41:14 +0430

    Python GUI

---
+++ 3.3_tkinter/tkinter_1.py
@@ -0,0 +1,29 @@
+# Introducing the "tkinter" , best choice available for python's GUI
+import tkinter
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Hello Dude !")
+mainWindow.geometry('640x480')
+
+label = tkinter.Label(mainWindow, text="Hello Dude !")
+label.pack(side='top')
+
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.pack(side='left', anchor='n')
+
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.pack(side='right', anchor='n', expand=True)
+
+button1 = tkinter.Button(rightFrame, text="button1")
+button2 = tkinter.Button(rightFrame, text="button2")
+button3 = tkinter.Button(rightFrame, text="button3")
+button1.pack(side='top')
+button2.pack(side='top')
+button3.pack(side='top')
+
+
+mainWindow.mainloop()
